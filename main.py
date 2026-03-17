fa2148ac29f8dfcd7c91d797841022b935e9d8cd
shawnyan-01 <85020496+shawnyan-01@users.noreply.github.com>
2026-03-17 22:41:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from client_server.game_agent_server import GameServerAgent
 import asyncio
 if __name__ == "__main__":
     
-    server = GameServerAgent(host="0.0.0.0", port=9495)
+    server = GameServerAgent(host="0.0.0.0", port=8080)
     asyncio.run(server.start())
 
